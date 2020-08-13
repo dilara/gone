1ec695808b2e72d9a0dd64b57e59be70353f8bca
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-08-13 21:31:16 +0300

    Set layout by resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
   protected
+
+  def layout_by_resource
+    return 'devise' if devise_controller? && (resource_name == :user) && %w[new create].include?(action_name)
+
+    'application'
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
