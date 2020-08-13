b92a2af86a2dae7b4c7ed9cba9a78649ae9644fd
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-08-13 21:28:13 +0300

    Change some devise path names

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  devise_for :users
+  devise_for :user, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 end
