78ba1c3422e07fe2abe5964fb3a83d216495f361
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-08-14 22:26:46 +0300

    Add callback for seting random password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Callbacks
+  before_validation :set_random_password, on: :create, if: Proc.new { |user| !user.customer? }
+
   # Enums
   enum status: { active: 0, inactive: 1 }
   enum role: { customer: 0, admin: 1 }
@@ -16,4 +19,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
   validates :role, inclusion: { in: roles.keys }
   validates :status, inclusion: { in: statuses.keys }
+
+  private
+
+  def set_random_password
+    self.password = Devise.friendly_token.first(8)
+  end
 end
