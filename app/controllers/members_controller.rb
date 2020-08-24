c8fd6d2de8d2ca2c207dd00ba823b42e8b6dfd7e
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-08-24 18:24:34 +0300

    Delete trailing whitespaces

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,11 +2,11 @@
 
 class MembersController < ApplicationController
   before_action :set_member, only: :show
-  
+
   def show; end
-  
+
   private
-  
+
   def set_member
     member = User.member.find(params[:id])
     @member = UserDecorator.new(member)
