edd5fec5bd40d702c746bbed99faf67fbd180fbc
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-08-26 20:23:24 +0300

    Validate cover presence in auction validator

--- app/validators/auction_validator.rb
+++ app/validators/auction_validator.rb
@@ -2,11 +2,21 @@
 
 class AuctionValidator < ActiveModel::Validator
   def validate(record)
-    now = Time.zone.now
-    record.errors.add(:starts_at, "can't be in the past") if record.expires_at < now
-    record.errors.add(:expires_at, "can't be in the past") if record.starts_at < now
-    return unless record.expires_at <= record.starts_at
+    @record = record
+    validate_dates
+    validate_cover_presence
+  end
 
-    record.errors.add(:expires_at, "can't be earlier than starts at")
+  private
+
+  def validate_dates
+    now = Time.zone.now
+    @record.errors.add(:starts_at, "can't be in the past") if @record.starts_at < now
+    @record.errors.add(:expires_at, "can't be in the past") if @record.expires_at < now
+    @record.errors.add(:expires_at, "can't be earlier than starts at") if @record.expires_at <= @record.starts_at
+  end
+
+  def validate_cover_presence
+    @record.errors.add(:cover, 'no file added') unless @record.cover.attached?
   end
 end
