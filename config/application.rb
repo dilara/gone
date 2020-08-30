1c997b32c89bd6b003422f99c6229b9f1f3834b2
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-08-30 12:13:09 +0300

    Set timezone

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Gone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # time-zone. ActiveSupport::TimeZone.all for all possible values
+    config.time_zone = 'Istanbul'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
