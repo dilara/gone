41ff260f00434dc8babbbc75cb0bdb0135ccb3ab
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-10-17 10:27:48 +0300

    Fix auction validation

--- app/validators/auction_validator.rb
+++ app/validators/auction_validator.rb
@@ -3,18 +3,11 @@
 class AuctionValidator < ActiveModel::Validator
   def validate(record)
     @record = record
-    validate_dates
+    @record.errors.add(:expires_at, "can't be earlier than starts at") if @record.expires_at <= @record.starts_at
     validate_cover_presence
   end
 
   private
-
-  def validate_dates
-    now = Time.zone.now
-    @record.errors.add(:starts_at, "can't be in the past") if @record.starts_at < now
-    @record.errors.add(:expires_at, "can't be in the past") if @record.expires_at < now
-    @record.errors.add(:expires_at, "can't be earlier than starts at") if @record.expires_at <= @record.starts_at
-  end
 
   def validate_cover_presence
     return @record.errors.add(:cover, 'no file added') unless @record.cover.attached?
